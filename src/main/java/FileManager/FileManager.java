aa6630c51cecc6cef0564aa5fdad10428a74d199
dana <danotbhd@gmail.com>
2018-11-08 11:15:41 +0200

    Started Indexing

--- src/main/java/FileManager/FileManager.java
+++ src/main/java/FileManager/FileManager.java
@@ -32,17 +32,18 @@ public class FileManager {
             Cache.put(key, "|" + DocId + "," + value.toString());
             Q.add(new PointerCache(key, PriorityAll));
         }
-        if (Cache.size() > 500) {
+        PriorityAll++;
+        if (Cache.size() > 10000) {
             PointerCache keytofile = Q.poll();
             String Value = Cache.get(keytofile.pointer);
             Cache.remove(keytofile.pointer);
-            File file =new File("src\\main\\java\\Parser\\Terms\\"+"\\" + keytofile.pointer + ".txt");
+            File file =new File(geturl(keytofile.pointer));
             try {
                 file.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            try (FileWriter fw = new FileWriter("src\\main\\java\\Parser\\Terms\\" +"\\"+ keytofile.pointer + ".txt", true);
+            try (FileWriter fw = new FileWriter(geturl(keytofile.pointer), true);
                  BufferedWriter bw = new BufferedWriter(fw);
                  PrintWriter out = new PrintWriter(bw)) {
                 out.print(Value);
@@ -50,6 +51,57 @@ public class FileManager {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static String geturl(String pointer) {
+        char first =pointer.charAt(0);
+        switch (first){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '$':
+                return "Indexing\\Numbers\\"+pointer+".txt";
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+                return "Indexing\\a-e\\"+pointer+".txt";
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+                return "Indexing\\f-j\\"+pointer+".txt";
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+                return "Indexing\\k-o\\"+pointer+".txt";
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+                return "Indexing\\p-t\\"+pointer+".txt";
+            case 'u':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'y':
+            case 'z':
+                return "Indexing\\u-z\\"+pointer+".txt";
+        }
+
+   return null;
     }
 
     public void setDocId(String docId) {
---
+++ src/main/java/Indexer/Indexer.java
@@ -0,0 +1,115 @@
+package Indexer;
+import FileManager.FileManager;
+
+import java.io.*;
+import java.util.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.TreeMap;
+
+import static FileManager.FileManager.geturl;
+
+public class Indexer {
+
+    TreeMap <String,String []> Index;
+
+    public Indexer() {
+        Index = new TreeMap<> ();
+
+    }
+
+
+    public void Index(){
+        File folder = new File("Terms");
+        File[] ListOfFile= folder.listFiles();
+        for (int i = 0; i <ListOfFile.length ; i++) {
+
+       IndexFile(ListOfFile[i]);
+
+        }
+
+
+
+    }
+
+    private void IndexFile(File file) {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get( "Terms\\" + file.getName())), Charset.defaultCharset());
+            String [] Parmeters= content.split("|");
+            qSort(Parmeters,0,Parmeters.length-1);
+            StringJoiner sj=new StringJoiner("|");
+            for (int i = 0; i <Parmeters.length ; i++) {
+                sj.add(Parmeters[i]);
+
+
+            }
+            try (FileWriter fw = new FileWriter(geturl(file.getName()), false);
+                 BufferedWriter bw = new BufferedWriter(fw);
+                 PrintWriter out = new PrintWriter(bw)) {
+                out.print(sj);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+    // Java program for implementation of QuickSort
+        /* This function takes last element as pivot,
+        places the pivot element at its correct
+        position in sorted array, and places all
+        smaller (smaller than pivot) to left of
+        pivot and all greater elements to right
+        of pivot */
+         int partition(String arr[], int low, int high)
+        {
+            int pivot =Integer.parseInt(arr[high].split(",")[1]);
+            int i = (low-1); // index of smaller element
+            for (int j=low; j<=high-1; j++)
+            {
+                // If current element is smaller than or
+                // equal to pivot
+                if (Integer.parseInt(arr[j].split(",")[1]) <= pivot)
+                {
+                    i++;
+
+                    // swap arr[i] and arr[j]
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot)
+            String temp = arr[i+1];
+            arr[i+1] = arr[high];
+            arr[high] = temp;
+
+            return i+1;
+        }
+
+        /* The main function that implements QuickSort()
+        arr[] --> Array to be sorted,
+        low --> Starting index,
+        high --> Ending index */
+        void qSort(String arr[], int low, int high)
+        {
+            if (low < high)
+            {
+            /* pi is partitioning index, arr[pi] is
+            now at right place */
+                int pi = partition(arr, low, high);
+
+                // Recursively sort elements before
+                // partition and after partition
+                qSort(arr, low, pi-1);
+                qSort(arr, pi+1, high);
+            }
+        }
+    }
